85c64f55a70033044ff06f38e5fb620fbe631eb2
Mateus Henrique <mateuzhenriq3@gmail.com>
2025-03-30 23:45:36 -0300

    Alteração 5

--- ProjetoBanco/src/App.java
+++ ProjetoBanco/src/App.java
@@ -22,26 +22,5 @@ public class App {
         p2.sacar(500);
         p2.fecharConta();
         p2.estadoAtual(); 
-
-        ContaBanco p3 = new ContaBanco(0, false);
-        p3.setnumConta(1290);
-        p3.setDono("Isaque");
-        p3.abrirConta("CC");
-        p3.depositar(7500);
-        p3.estadoAtual(); 
-
-        ContaBanco p4 = new ContaBanco(0, false);
-        p4.setnumConta(1290);
-        p4.setDono("Heverton");
-        p4.abrirConta("CP");
-        p4.depositar(15000);
-        p4.estadoAtual(); 
-
-        ContaBanco p5 = new ContaBanco(0, false);
-        p5.setnumConta(1290);
-        p5.setDono("Verônica");
-        p5.abrirConta("CP");
-        p5.depositar(35000);
-        p5.estadoAtual(); 
     }
 }
